15178506aacfec255e949e85e63874fef0ee5421
botnaga <89389477+botnaga@users.noreply.github.com>
2022-07-04 16:29:49 +0700

    Update info-sc.js

--- plugins/info-sc.js
+++ plugins/info-sc.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch'
 let handler = async(m, { conn, text, usedPrefix, command }) => {
 let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
 
-let str = `*GK ADA SC SC AN CODING SENDIRI SONO*\n© _Anggara_`
+let str = `*BELI SC MURAH DI KAMI*\n© _BOT-PLAYSHOP_`
 conn.sendButtonDoc(m.chat, str, wm, 'Owner','.owner', ftextt, m)
 }
 handler.command = handler.help = ['sc']
